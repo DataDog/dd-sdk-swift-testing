808e134b774a6eee39c243e7ffa437d50cfff875
Nacho Bonafonte <nacho.bonafontearruga@datadoghq.com>
2020-10-30 11:38:11 +0100

    CIAPP-169 - Support UI tests - Apply suggestions from code review

--- Sources/DatadogSDKTesting/Utils/XCUIApplicationSwizzler.swift
+++ Sources/DatadogSDKTesting/Utils/XCUIApplicationSwizzler.swift
@@ -8,6 +8,10 @@ import Foundation
 @_implementationOnly import XCTest
 
 extension XCUIApplication {
+    fileprivate func addProcessEnvironmentToLaunch(_ environment: String) {
+        self.launchEnvironment[environment] = ProcessInfo.processInfo.environment[environment]
+    }
+
     static let swizzleMethods: Void = {
         guard let originalMethod = class_getInstanceMethod(XCUIApplication.self, #selector(launch)),
         let swizzledMethod = class_getInstanceMethod(XCUIApplication.self, #selector(swizzled_launch))
@@ -19,16 +23,18 @@ extension XCUIApplication {
     func swizzled_launch() {
         if let testSpanContext = DDTestMonitor.instance?.tracer.activeTestSpan?.context {
             self.launchEnvironment["DD_TEST_RUNNER"] = "1"
-            self.launchEnvironment["DATADOG_CLIENT_TOKEN"] = ProcessInfo.processInfo.environment["DATADOG_CLIENT_TOKEN"]
-            self.launchEnvironment["XCTestConfigurationFilePath"] = ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"]
             self.launchEnvironment["ENVIRONMENT_TRACER_SPANID"] = testSpanContext.spanId.hexString
             self.launchEnvironment["ENVIRONMENT_TRACER_TRACEID"] = testSpanContext.traceId.hexString
-            self.launchEnvironment["DD_ENV"] = ProcessInfo.processInfo.environment["DD_ENV"]
-            self.launchEnvironment["DD_SERVICE"] = ProcessInfo.processInfo.environment["DD_SERVICE"]
-            self.launchEnvironment["DD_DISABLE_NETWORK_INSTRUMENTATION"] = ProcessInfo.processInfo.environment["DD_DISABLE_NETWORK_INSTRUMENTATION"]
-            self.launchEnvironment["DD_DISABLE_HEADERS_INJECTION"] = ProcessInfo.processInfo.environment["DD_DISABLE_HEADERS_INJECTION"]
-            self.launchEnvironment["DD_DISABLE_STDOUT_INSTRUMENTATION"] = ProcessInfo.processInfo.environment["DD_DISABLE_STDOUT_INSTRUMENTATION"]
-            self.launchEnvironment["DD_DISABLE_STDERR_INSTRUMENTATION"] = ProcessInfo.processInfo.environment["DD_DISABLE_STDERR_INSTRUMENTATION"]
+            [
+                "DATADOG_CLIENT_TOKEN",
+                "XCTestConfigurationFilePath",
+                "DD_ENV",
+                "DD_SERVICE",
+                "DD_DISABLE_NETWORK_INSTRUMENTATION",
+                "DD_DISABLE_HEADERS_INJECTION",
+                "DD_DISABLE_STDOUT_INSTRUMENTATION",
+                "DD_DISABLE_STDERR_INSTRUMENTATION"
+            ].forEach( addProcessEnvironmentToLaunch )
         }
         swizzled_launch()
     }

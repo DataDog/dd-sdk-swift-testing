a2aa1eaae8564afc27623f2df8dbcd8a596b5e9d
Ignacio Bonafonte <nacho.bonafontearruga@datadoghq.com>
2021-04-29 17:33:46 +0200

    Symbols output was too big to use pipes for redirecting output, use a temporary file instead

--- Sources/DatadogSDKTesting/Crashes/DDSymbolicator.swift
+++ Sources/DatadogSDKTesting/Crashes/DDSymbolicator.swift
@@ -309,8 +309,13 @@ enum DDSymbolicator {
     #else
     static func symbolsInfo(forLibrary library: String) -> String? {
         if let imagePath = dSYMFiles.first(where: { $0.lastPathComponent == library })?.path {
-            let dwarfInfo = Spawn.commandWithResult("/usr/bin/symbols -fullSourcePath -lazy \(imagePath)")
-            return dwarfInfo
+            let symbolsOutputURL = URL(fileURLWithPath: NSTemporaryDirectory())
+                .appendingPathComponent("symbols_output")
+            FileManager.default.createFile(atPath: symbolsOutputURL.path, contents: nil, attributes: nil)
+            Spawn.commandToFile("/usr/bin/symbols -fullSourcePath -lazy \(imagePath)", outputPath: symbolsOutputURL.path)
+            defer{ try? FileManager.default.removeItem(at: symbolsOutputURL) }
+            let outputData = try? String(contentsOf: symbolsOutputURL)
+            return outputData
         } else {
             return nil
         }

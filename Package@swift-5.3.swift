c00df60b1ed6efb7a3d17ac3635be646678deca0
Ignacio Bonafonte <nacho.bonafontearruga@datadoghq.com>
2021-06-28 17:25:42 +0200

    CIAPP-2153 Add binary distribution for SPM Created a Package.swift for Xcode 12 and up so it automatically downloads the binary

---
+++ Package@swift-5.3.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let releaseVersion = "0.8.2"
+let relaseChecksum = "6e26edbb02b460e71f801ca624b185291c3b5ed7093a7ecce6fa84b6d1e8604a"
+let url = "https://github.com/DataDog/dd-sdk-swift-testing/releases/download/\(releaseVersion)/DatadogSDKTesting.zip"
+
+let package = Package(
+    name: "dd-sdk-swift-testing",
+    platforms: [.macOS(.v10_13),
+                .iOS(.v11),
+                .tvOS(.v11)],
+    products: [
+        .library(
+            name: "DatadogSDKTesting",
+            targets: [
+                "DatadogSDKTesting",
+            ]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "DatadogSDKTesting",
+            url: url,
+            checksum: relaseChecksum
+        )
+    ]
+)

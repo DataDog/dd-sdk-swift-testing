15d3d72b153ae9b03609a9dfa13c66401b362788
Ignacio Bonafonte <nacho.bonafontearruga@datadoghq.com>
2021-11-25 17:33:41 +0100

    Remove FileLocator.filePath method using "atos" that was not used anywhere Modify FileLocatorTests so it uses the current implementation for source location

--- Tests/DatadogSDKTesting/Utils/FileLocatorTests.swift
+++ Tests/DatadogSDKTesting/Utils/FileLocatorTests.swift
@@ -11,20 +11,15 @@ import XCTest
 internal class FileLocatorTests: XCTestCase {
     #if !os(tvOS)
         func testThisTestLocation() {
-            let className = object_getClassName(self)
-            let bundleName = Bundle(for: type(of: self)).bundleURL.deletingPathExtension().lastPathComponent
+            let testName = "FileLocatorTests.testThisTestLocation"
+            let bundleName = Bundle(for: FileLocatorTests.self).bundleURL.deletingPathExtension().lastPathComponent
+            DDSymbolicator.createDSYMFileIfNeeded(forImageName: bundleName)
 
-            let testNameRegex = try! NSRegularExpression(pattern: "([\\w]+) ([\\w]+)", options: .caseInsensitive)
-            let namematch = testNameRegex.firstMatch(in: self.name, range: NSRange(location: 0, length: self.name.count))
-            let nameRange = Range(namematch!.range(at: 2), in: self.name)
-            let testName = String(self.name[nameRange!])
-
-            let testSourcePath = FileLocator.filePath(forTestClass: className, testName: testName, library: bundleName)
-
-            XCTAssertFalse(testSourcePath.isEmpty)
-            let sourceComponents = testSourcePath.components(separatedBy: ":")
-            XCTAssertEqual(#file, sourceComponents[0])
-            XCTAssertEqual("13", sourceComponents[1])
+            let bundleFunctionInfo = FileLocator.testFunctionsInModule(bundleName)
+            let functionInfo = bundleFunctionInfo[testName]
+            XCTAssertEqual(#file, functionInfo?.file)
+            XCTAssertEqual(13, functionInfo?.startLine)
+            XCTAssertEqual(23, functionInfo?.endLine)
         }
     #endif
 }

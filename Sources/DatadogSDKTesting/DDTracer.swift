af51945182ddab9e4ba851c59ed3576cd91c82e6
Ignacio Bonafonte <nacho.bonafontearruga@datadoghq.com>
2022-01-14 12:45:54 +0100

    Update opentelemetry-swift to version 1.1.2

--- Sources/DatadogSDKTesting/DDTracer.swift
+++ Sources/DatadogSDKTesting/DDTracer.swift
@@ -69,10 +69,7 @@ internal class DDTracer {
             case "us3", "US3", "https://us3.datadoghq.com", "us3.datadoghq.com":
                 endpoint = Endpoint.us3
             case "us5", "US5", "https://us5.datadoghq.com", "us5.datadoghq.com":
-                // endpoint = Endpoint.us5
-                endpoint = Endpoint.custom(tracesURL: URL(string: "https://trace.browser-intake-us5-datadoghq.com/api/v2/spans")!,
-                                           logsURL: URL(string: "https://logs.browser-intake-us5-datadoghq.com/api/v2/logs")!,
-                                           metricsURL: URL(string: "https://api.us5.datadoghq.com/api/v1/series")!)
+                endpoint = Endpoint.us5
             case "eu", "EU", "eu1", "EU1", "https://app.datadoghq.eu", "app.datadoghq.eu", "datadoghq.eu":
                 endpoint = Endpoint.eu1
             case "gov", "GOV", "us1_fed", "US1_FED", "https://app.ddog-gov.com", "app.ddog-gov.com", "ddog-gov.com":

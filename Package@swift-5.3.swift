333332f296794e186ceb46d092e319944fc2a0d9
runner <runner@Mac-1642166958153.local>
2022-01-14 13:47:11 +0000

    Updated binary package version to 1.1.0

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let releaseVersion = "1.0.3-beta.1"
-let relaseChecksum = "ae44595e78d9a16665d7bc5e9d629a1dfcf4dfa566f17c327fe8bf191782369d"
+let releaseVersion = "1.1.0"
+let relaseChecksum = "92d428623f263506e7d89d05192886cbee1e488f8d51cf148a26eb6eee3eae9a"
 let url = "https://github.com/DataDog/dd-sdk-swift-testing/releases/download/\(releaseVersion)/DatadogSDKTesting.zip"
 
 let package = Package(

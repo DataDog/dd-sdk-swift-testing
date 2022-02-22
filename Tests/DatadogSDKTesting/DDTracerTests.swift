6eb4a1006707bf720f0b4add1bec999e6b808b8c
Ignacio Bonafonte <nacho.bonafontearruga@datadoghq.com>
2022-02-22 13:45:07 +0100

    Fix test that was evaluating old behaviour

--- Tests/DatadogSDKTesting/DDTracerTests.swift
+++ Tests/DatadogSDKTesting/DDTracerTests.swift
@@ -264,10 +264,8 @@ class DDTracerTests: XCTestCase {
         XCTAssert(environmentValues["TRACEPARENT"]?.contains(spanData.traceId.hexString) ?? false)
         XCTAssertTrue(environmentValues["TRACEPARENT"]?.contains(spanData.spanId.hexString) ?? false)
 
-        XCTAssertNotNil(environmentValues[DDHeaders.traceIDField.rawValue])
-        XCTAssertEqual(environmentValues[DDHeaders.traceIDField.rawValue], String(spanData.traceId.rawLowerLong))
-        XCTAssertNotNil(environmentValues[DDHeaders.parentSpanIDField.rawValue])
-        XCTAssertEqual(environmentValues[DDHeaders.parentSpanIDField.rawValue], String(spanData.spanId.rawValue))
+        XCTAssertNotNil(environmentValues["CI_VISIBILITY_TEST_EXECUTION_ID"])
+        XCTAssertEqual(environmentValues["CI_VISIBILITY_TEST_EXECUTION_ID"], String(spanData.traceId.rawLowerLong))
 
         span.end()
     }

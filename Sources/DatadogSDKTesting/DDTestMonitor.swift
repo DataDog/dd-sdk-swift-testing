75867562e200e26c3bcb4d5165ceed02b12ccc8d
Ignacio Bonafonte <nacho.bonafontearruga@datadoghq.com>
2022-05-13 09:51:27 +0200

    CIAPP-4068 update logic to fail the process if API key is missing

--- Sources/DatadogSDKTesting/DDTestMonitor.swift
+++ Sources/DatadogSDKTesting/DDTestMonitor.swift
@@ -47,8 +47,8 @@ internal class DDTestMonitor {
 
     static func installTestMonitor() {
         guard DDEnvironmentValues.getEnvVariable("DD_API_KEY") != nil else {
-            Log.print("DD_API_KEY is missing.")
-            return
+            Log.print("A Datadog API key is required. DD_API_KEY environment value is missing.")
+            exit(1)
         }
         if DDEnvironmentValues.getEnvVariable("SRCROOT") == nil {
             Log.print("SRCROOT is not properly set")

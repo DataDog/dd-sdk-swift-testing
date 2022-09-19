84e4a7b36e43cba2b9d577e74cb655c08d4fdc69
Ignacio Bonafonte <nacho.bonafontearruga@datadoghq.com>
2022-09-19 15:28:00 +0200

    Fix spawn tests now that we dont support environment values

--- Tests/EventsExporter/Utils/SpawnTests.swift
+++ Tests/EventsExporter/Utils/SpawnTests.swift
@@ -3,18 +3,18 @@ import XCTest
 
 class SpawnTests: XCTestCase {
     func testSpawnCommandRuns() {
-        Spawn.command("echo $VALUE", environment: ["VALUE": "Hello World"])
+        Spawn.command("echo Hello World")
     }
 
     func testSpawnCommandWithResult() {
-        let output = Spawn.commandWithResult("echo $VALUE", environment: ["VALUE": "Hello World"])
+        let output = Spawn.commandWithResult("echo Hello World")
         XCTAssertEqual(output, "Hello World\n")
     }
 
     func testSpawnCommandToFile() throws {
         let tempURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString)
         FileManager.default.createFile(atPath: tempURL.path, contents: nil, attributes: nil)
-        Spawn.commandToFile("echo $VALUE", outputPath: tempURL.path, environment: ["VALUE": "Hello World"])
+        Spawn.commandToFile("echo Hello World", outputPath: tempURL.path)
         let writtenData = try String(contentsOf: tempURL, encoding: .ascii)
         XCTAssertEqual(writtenData, "Hello World\n")
     }

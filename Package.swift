4cadebe27d7ac4c21c33057fe7dc1f96bcef1303
Nacho Bonafonte <nacho.bonafontearruga@datadoghq.com>
2023-02-24 10:05:44 +0100

    Bumped version to 2.2.3

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "opentelemetry-swift", url: "https://github.com/open-telemetry/opentelemetry-swift", from: "1.0.7"),
-        .package(name: "PLCrashReporter", url: "https://github.com/microsoft/plcrashreporter.git", from: "1.9.0"),
+        .package(name: "opentelemetry-swift", url: "https://github.com/open-telemetry/opentelemetry-swift", from: "1.4.0"),
+        .package(name: "PLCrashReporter", url: "https://github.com/microsoft/plcrashreporter.git", from: "1.11.0"),
         .package(name: "SigmaSwiftStatistics", url: "https://github.com/evgenyneu/SigmaSwiftStatistics.git", from: "9.0.2")
     ],
     targets: [

797df57b2391f16c5c68c2d3c3ff301d1cc64dcf
Nacho Bonafonte <nacho.bonafontearruga@datadoghq.com>
2023-03-02 10:31:17 +0100

    Update Copyright dates

--- Sources/DatadogSDKTesting/Utils/CodeOwners.swift
+++ Sources/DatadogSDKTesting/Utils/CodeOwners.swift
@@ -1,9 +1,8 @@
-//
-//  CodeOwner.swift
-//  DatadogSDKTesting
-//
-//  Created by Ignacio Bonafonte Arruga on 15/6/21.
-//
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2020-Present Datadog, Inc.
+ */
 
 import Foundation
 

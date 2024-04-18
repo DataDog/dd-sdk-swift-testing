24f9da759a58d8277631ab2dcf8491738c6acecc
Yehor Popovych <yehor.popovych@datadoghq.com>
2024-04-18 15:56:47 +0100

    switched binary to own version

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 
 let releaseVersion = "2.4.0"
-let relaseChecksum = "6789599ea4013d8aeadd201ea6e8ba30a0565a0eceb9130fda60e249e262a43a"
-let url = "https://github.com/DataDog/dd-sdk-swift-testing/releases/download/\(releaseVersion)/DatadogSDKTesting.zip"
+let relaseChecksum = "c6a92de0407dc8ed42da36f9c15795e2d2eac66b5f16f82d0a22926060f64bfa"
+let url = "https://popovy.ch/wp-content/uploads/2024/04/DatadogSDKTesting.xcframework.zip"
 
 let package = Package(
     name: "dd-sdk-swift-testing",

177ff840a6c2b7b87d1c7c64ecd8472344ae759f
Yehor Popovych <ypopovych@users.noreply.github.com>
2024-08-21 21:26:38 +0100

    fixed test for file locator (#126)

--- Tests/DatadogSDKTesting/Utils/FileLocatorTests.swift
+++ Tests/DatadogSDKTesting/Utils/FileLocatorTests.swift
@@ -9,15 +9,19 @@ import Foundation
 import XCTest
 
 internal class FileLocatorTests: XCTestCase {
-    func testThisTestLocation() {
+    func testThisTestLocation() throws {
         let testName = "FileLocatorTests.testThisTestLocation"
         let bundleName = Bundle(for: FileLocatorTests.self).bundleURL.deletingPathExtension().lastPathComponent
+        
+        try FileManager.default.createDirectory(at: DDSymbolicator.dsymFilesDir.url,
+                                                withIntermediateDirectories: true)
+        
         DDSymbolicator.createDSYMFileIfNeeded(forImageName: bundleName)
 
         let bundleFunctionInfo = FileLocator.testFunctionsInModule(bundleName)
         let functionInfo = bundleFunctionInfo[testName]
         XCTAssertEqual(#file, functionInfo?.file)
         XCTAssertEqual(12, functionInfo?.startLine)
-        XCTAssertEqual(22, functionInfo?.endLine)
+        XCTAssertEqual(26, functionInfo?.endLine)
     }
 }

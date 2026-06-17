84e5d4bdd60fd78aaec7261ff11d6ee46845859d
Yehor Popovych <yehor.popovych@datadoghq.com>
2026-06-17 14:40:55 +0100

    increase priorities of the setup queues

--- Sources/DatadogSDKTesting/DDTestMonitor.swift
+++ Sources/DatadogSDKTesting/DDTestMonitor.swift
@@ -153,6 +153,13 @@ internal class DDTestMonitor {
         maxPayloadSize = DDTestMonitor.config.maxPayloadSize
         messageChannelUUID = DDTestMonitor.config.messageChannelUUID ?? UUID().uuidString
         tracer = DDTracer()
+        
+        // Change queue priorities. We want loading to work quick
+        gitUploadQueue.qualityOfService = .userInteractive
+        instrumentationWorkQueue.qualityOfService = .userInteractive
+        testOptimizationSetupQueue.qualityOfService = .userInteractive
+        
+        // Make git queue serial. We can't run multiple git commands at the same time
         gitUploadQueue.maxConcurrentOperationCount = 1
 
         if DDTestMonitor.config.isBinaryUnderUITesting {
